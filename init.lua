d860cb3beab36d68bf8f47f3fb3ebe5a2d69e595
OrlandoDomo <55752427+OrlandoDomo@users.noreply.github.com>
2025-05-13 12:58:21 -0500

    Added pyright and tinymist lsps

--- init.lua
+++ init.lua
@@ -686,9 +686,14 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        tinymist = {
+          settings = {
+            formatterMode = 'typstyle',
+            exportPdf = 'onType',
+            semanticTokens = 'disable',
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
